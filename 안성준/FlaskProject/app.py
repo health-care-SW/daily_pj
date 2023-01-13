ee1ee8046ede51bce125e91a83d167091f23509b
sungjun-an <hiahn777@naver.com>
2023-01-13 16:44:58 +0900

    POST 진행하려고 합니다.

--- 안성준/FlaskProject/app.py
+++ 안성준/FlaskProject/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'hello'
+
+@app.route('/sql')
+def index():
+    return render_template('index.html')
+
+@app.route('/command',methods=['POST'])
+def sql():
+    return "TEST" + request.form.get('first_test')
 
 if __name__=="__main__":
     app.run()

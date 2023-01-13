112726b41fca3ab794a64751b8877bf4afe0a05a
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2023-01-13 16:45:25 +0900

    add index.html

--- 김지훈/Health_Care_Flask/app.py
+++ 김지훈/Health_Care_Flask/app.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def hello():
+    return 'hello world'
+
+
+@app.route('/sql')
+def index():
+    return render_template("index.html")
+
+@app.route('/command', methods=['POST'])
+def command():
+    getStr = request.form.get('first_test')
+
+    if '+' in getStr: 
+        a,b = getStr.split('+')
+        res = int(a) + int(b)
+
+        return 'command: ' + str(res)
+    else:
+        return 'invalid command'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

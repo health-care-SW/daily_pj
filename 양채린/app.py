ded0b78b8e75ce2f43747c5aeca2651f2feb3134
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-01-14 00:44:33 +0900

    Day02

--- 양채린/app.py
+++ 양채린/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+app = Flask(__name__) # Flask라는 클래스를 app이라는 인스턴스에 받는 것이다.
 
-@app.route('/)
-           def hello_world():
-            return "Hello World"
+@app.route('/') # 이 경로를 호출하면 아래의 함수를 호출하여라.
+def hello_world():
+    return "Hello World"
+
+@app.route('/sql')
+def index():
+    return render_template("index.html")
+
+@app.route('/command', methods=['POST'])
+def test():
+    return "TEST"+request.form.get('first_test')
            
 if __name__ == "__main__":
-           app.run()
+    app.run(debug=True)

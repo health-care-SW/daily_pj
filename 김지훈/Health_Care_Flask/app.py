7c061c9b162961173445e993edd51dfab0127455
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2023-01-14 16:24:06 +0900

    db 사용

--- 김지훈/Health_Care_Flask/app.py
+++ 김지훈/Health_Care_Flask/app.py
@@ -1,4 +1,25 @@
 from flask import Flask, render_template, request
+import sqlite3
+import pandas as pd
+
+def get_db(db_name):
+    return sqlite3.connect(db_name)
+
+def exe_sql(conn, command):
+
+    try:
+        conn.execute(command)
+        conn.commit()
+
+        if command.split()[0].lower() == 'select':
+            df = pd.read_sql(command, conn, index_col = None)
+            conn.close()
+            return df, 1
+        else:
+            return None, 0
+    except:
+        return None, 0
+
 
 app = Flask(__name__)
 
@@ -22,6 +43,32 @@ def command():
         return 'command: ' + str(res)
     else:
         return 'invalid command'
+
+@app.route('/data')
+def data_page():
+    return render_template("data.html")
+
+
+
+@app.route('/dbsql', methods=['POST'])
+def sql_test():
+    if request.method=='POST':
+        db_name = request.form.get('db_name')
+        sql_command = request.form.get('sql')
+
+        conn = get_db(db_name)
+        output, status= exe_sql(conn, sql_command)
+
+        if status == 1:
+            # sql 실행 성공
+            return render_template('data.html', label = "정상 작동", outputs=[output.to_html(classes='data')])
+            #return output
+
+        else:
+            # sql 실행 오류
+            return render_template('data.html', label = "오류 발생" ,output = None
+)
+
 
 
 if __name__ == "__main__":

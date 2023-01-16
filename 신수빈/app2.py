0198c161759e995c9eeb15e958a4ae35acf575ad
ongsim0629 <99964694+ongsim0629@users.noreply.github.com>
2023-01-16 17:07:40 +0900

    3일차

--- 신수빈/app2.py
+++ 신수빈/app2.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request
-from flask import Blueprint
 import os
 from PIL import Image
 
-app = Blueprint("image",__name__, url_prefix="/app_image")
+app = Flask(__name__)
 
 '''
 이미지 처리 함수
 '''
 def image_resize(image, width, height):
-        return image.resize((int(width), int(height)))
+    return image.resize((int(width), int(height)))
 
 def image_rotate(image):
     return image.transpose(Image.ROTATE_180)
@@ -17,19 +16,22 @@ def image_rotate(image):
 def image_change_bw(image):
     return image.convert('L')
 
+
+
 '''
 플라스크
 '''
 @app.route("/")
 def index():
-    return render_template('image.html')
+    return render_template('index2.html')
 
 @app.route('/image_preprocess', methods=['POST'])
 def preprocessing():
     if request.method == 'POST':
-        img_name = request.form['label1']
+        file = request.files['uploaded_image']
+        if not file: return render_template('index.html', label="No Files")
 
-        img = Image.open('main/static/'+img_name)
+        img = Image.open(file)
 
         is_rotate_180 = request.form.get('pre_toggle_0')
         is_change_bw = request.form.get('pre_toggle_1')
@@ -44,17 +46,13 @@ def preprocessing():
         if is_change_size == 'on':
             img = image_resize(img, request.form.get('changed_width'), request.form.get('changed_height'))
 
-        img.save('main/static/images/changed/'+img_name.split('/')[1])
-        image_path = 'images/changed/'+img_name.split('/')[1]
+        img.save('result_image.png')
 
-        # 결과 리턴
-        return render_template('image.html', label= img_name, labeling=image_path)
+        src_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(src_dir, 'result_image.png')
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image_file():
-    if request.method == 'POST':
-        file = request.files['uploaded_image']
-        if not file: return render_template('image.html', label='no file')
-        label = file.filename
-        file.save('main/static/images/' + file.filename)
-    return render_template('image.html', label='images/'+label)
+        return render_template('index.html', label=image_path)
+
+if __name__ == '__main__':
+    app.run()
+

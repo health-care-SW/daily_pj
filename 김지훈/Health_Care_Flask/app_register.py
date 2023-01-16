dbc0a5235166294dc8103904a84f9d3bf28939c2
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2023-01-16 16:52:07 +0900

    회원가입

--- 김지훈/Health_Care_Flask/app_register.py
+++ 김지훈/Health_Care_Flask/app_register.py
@@ -1,13 +1,22 @@
-import DB_CONNECT
+from flask import Flask, render_template, request
+import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("register.html")
 
 
+@app.route('/register', methods=["POST"])
+def register():
+    userName = request.form.get('user_name')
+    userPw = request.form.get('user_password')
+    userEmail = request.form.get('user_email')
+    user = User.User(userName, userPw, userEmail)
+    User.insert_user(user)
+    return render_template("register.html")
 
 if __name__ == "__main__":
-    conn, cur = DB_CONNECT.DB_connect()
-
-    sql = "SELECT * FROM users"
-    cur.execute(sql)
-    conn.commit()	# 저장
-    for u in cur.fetchall():
-        print(u)
-    conn.close()	# 종료
+    print(123)
+    app.run(debug=True)

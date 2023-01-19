0188533d043a1376631db6955ec1e96b62c98958
na0th <105775683+na0th@users.noreply.github.com>
2023-01-19 17:33:18 +0900

    01-19
    ...

---
+++ flask_project/test.py
@@ -0,0 +1,15 @@
+from db_connect import *
+
+# 데이터 베이스에 값 집어넣기
+
+# insert("2222","geon","abc","2222")
+# insert("1111","geon","abc","1111")
+# insert("3333","geon","abc","2222")
+# insert("4444","geon","abc","2222")
+# insert("5555","geon","abc","5555")
+# insert("6666","geon","abc","6666")
+# insert("1111","1111")
+# insert("2222","2222")
+
+# 데이터 베이스에서 값 가져오기
+print_rows()

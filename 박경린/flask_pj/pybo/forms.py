6aeadc025525f838aedce2f1707149a43eccd75f
pkl0912 <102944310+pkl0912@users.noreply.github.com>
2023-01-20 23:18:26 +0900

    Add files via upload

---
+++ 박경린/flask_pj/pybo/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired()])

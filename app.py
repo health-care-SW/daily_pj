a781f08f74c2ce7f4895fe18a6966c73c85e591f
eunjung0301 <102943250+eunjung0301@users.noreply.github.com>
2023-01-20 18:17:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,23 @@
-from flask import Flask, render_template, request
-# from flask_ngrok import run_with_ngrok
+import pymysql
+from flask import Flask, send_from_directory
+from api import board
+from db_connect import db
+from flask_bcrypt import Bcrypt
 import os
-from PIL import Image
 
 app = Flask(__name__)
-# run_with_ngrok(app)
+app.register_blueprint(board)
 
-'''
-이미지 처리 함수
-'''
-def image_resize(image, width, height):
-     return image.resize((int(width), int(height)))
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
-def image_rotate(image):
-    return image.transpose(Image.ROTATE_180)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://hchang:devpass@127.0.0.1:3306/board"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'ekdwls'
 
-def image_change_bw(image):
-    return image.convert('L')
-
-'''
-플라스크
-'''
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-@app.route('/image_preprocess', methods=['POST'])
-def preprocessing():
-    if request.method == 'POST':
-        file = request.files['uploaded_image']
-        if not file: return render_template('index.html', label="No Files")
-
-        img = Image.open(file)
-
-        is_rotate_180 = request.form.get('pre_toggle_0')
-        is_change_bw = request.form.get('pre_toggle_1')
-        is_change_size = request.form.get('pre_toggle_2')  
-        img.save(f'static/{file.filename}')
-
-        if is_rotate_180 == 'on':
-            img = image_rotate(img)
-
-        if is_change_bw == 'on':
-            img = image_change_bw(img)
-
-        if is_change_size == 'on':
-            img = image_resize(img, request.form.get('changed_width'), request.form.get('changed_height'))
-
-        img.save(f'static/changed_{file.filename}')
-
-
-        # 결과 리턴
-        return render_template('index.html', label=file.filename)
+db.init_app(app)
+bcrypt = Bcrypt(app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

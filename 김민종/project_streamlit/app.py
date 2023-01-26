4a2a60a1c5602fbccd3a886a19713af33d1365c9
minjong kim <alswhddkdl@gmail.com>
2023-01-26 16:18:07 +0900

    fix: streamlit-selenium location set

--- 김민종/project_streamlit/app.py
+++ 김민종/project_streamlit/app.py
@@ -11,9 +11,9 @@ import cv2
 import numpy as np
 import os
 from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager'
+# from webdriver_manager.chrome import ChromeDriverManager
 
-target_pill = ['뉴에르도테캡슐', '듀카브정30/10밀리그램', '듀카브정30/5밀리그램', '라'노펜세미정', '락토엔큐캡슐(바실루스리케니포르미스균)\xa0', '루키오정10밀리그램(몬테루카스트나트륨)', '리셀톤캡슐 6.0mg', '리프레가캡슐 75mg', '뮤코원캡슐(에르도스테인)', '바실리포미스캡슐', '베아로탄정 50mg', '베아투스정', '비오메틱스캡슐(바실루스리케니포르미스균)', '비우미정 500mg/병', '아나그레캡슐 0.5mg', '앤도민300프리미엄연질캡슐 300mg/PTP', '에피나레정', '엘도민캡슐 300mg', '엘도스인캡슐(에르도스테인)', '크라틴정 10mg', '크라틴정 20mg', '크라틴정 5mg', '티아프란정', '피타로틴정 2mg']
+target_pill = ['뉴에르도테캡슐', '듀카브정30/10밀리그램', '듀카브정30/5밀리그램', '라노펜세미정', '락토엔큐캡슐(바실루스리케니포르미스균)\xa0', '루키오정10밀리그램(몬테루카스트나트륨)', '리셀톤캡슐 6.0mg', '리프레가캡슐 75mg', '뮤코원캡슐(에르도스테인)', '바실리포미스캡슐', '베아로탄정 50mg', '베아투스정', '비오메틱스캡슐(바실루스리케니포르미스균)', '비우미정 500mg/병', '아나그레캡슐 0.5mg', '앤도민300프리미엄연질캡슐 300mg/PTP', '에피나레정', '엘도민캡슐 300mg', '엘도스인캡슐(에르도스테인)', '크라틴정 10mg', '크라틴정 20mg', '크라틴정 5mg', '티아프란정', '피타로틴정 2mg']
 # 약학정보원 약 코드
 drug_cd = ['2016052400013','2016053100044','2016053100043','2016051800012','2016051300001','2016053100033','2016052500022','2016052500003','2016051700009','2016051000018','2016053100001','2016053100005','2016051000004','2016051800001','2016051300003','2016051800004','2016050900002','2016051700013','2016051800009','2016051100002','2016051100001','2016051100003','2016050900008','2016053100020']
 
@@ -76,7 +76,7 @@ def get_text(ids, idx):
     keyword = ['result_drug_name', 'upso_title', 'charact','effect', 'dosage', 'caution']
     option = webdriver.ChromeOptions()
     option.add_argument("headless")
-    driver = webdriver.Chrome(service=Service(r'/app/daily_pj/김민종/project_streamlit/chromedriver'),options=option)
+    driver = webdriver.Chrome(service=Service('/app/daily_pj/김민종/project_streamlit/chromedriver'),options=option)
     driver.get("https://www.health.kr/searchDrug/result_drug.asp?drug_cd=%s"%(drug_cd[int(idx)]))
     for key in keyword:
         ids.append(driver.find_element(By.ID, key).text)

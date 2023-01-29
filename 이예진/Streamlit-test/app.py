e7d22ab859253e721efe0f7ff0ba6ad7c15fa8d5
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2023-01-29 22:36:27 +0900

    Add files via upload

--- 이예진/Streamlit-test/app.py
+++ 이예진/Streamlit-test/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import time
+import numpy as np
+import plotly.express as px
+
 
 st.write("Testing Streamlit")
 
@@ -30,3 +34,88 @@ conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
 df_gsheet = pd.DataFrame(rows)
 st.write(df_gsheet)
+
+
+#리얼타임 데이터셋 표시하기
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
+
+# 깃허브에서 csv 파일 읽어오기
+dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
+
+@st.experimental_memo
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(dataset_url)
+
+df = get_data()
+
+
+st.title("Real-Time / Live Data Science Dashboard")
+
+job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
+
+#컨테이너
+placeholder = st.empty()
+
+# dataframe filter
+df = df[df["job"] == job_filter]
+
+# near real-time / live feed simulation
+for seconds in range(200):
+
+    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
+    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+
+    # creating KPIs
+    avg_age = np.mean(df["age_new"])
+
+    count_married = int(
+        df[(df["marital"] == "married")]["marital"].count()
+        + np.random.choice(range(1, 30))
+    )
+
+    balance = np.mean(df["balance_new"])
+
+    with placeholder.container():
+
+        kpi1, kpi2, kpi3 = st.columns(3)
+
+        # fill in those three columns with respective metrics or KPIs
+        kpi1.metric(
+            label="Age ⏳",
+            value=round(avg_age),
+            delta=round(avg_age) - 10,
+        )
+        
+        kpi2.metric(
+            label="Married Count 💍",
+            value=int(count_married),
+            delta=-10 + count_married,
+        )
+        
+        kpi3.metric(
+            label="A/C Balance ＄",
+            value=f"$ {round(balance,2)} ",
+            delta=-round(balance / count_married) * 100,
+        )
+
+        # create two columns for charts
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(
+                data_frame=df, y="age_new", x="marital"
+            )
+            st.write(fig)
+            
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame=df, x="age_new")
+            st.write(fig2)
+
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
